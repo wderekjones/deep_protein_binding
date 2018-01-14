94d0f7a435b4118a5703dedbc2f955aeeca17a13
williamdjones <wdjo224@g.uky.edu>
2018-01-13 19:40:08 -0500

    working on processing and loading the data

---
+++ rename_molecules.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', type=str, help="path to directory containing data")
+args = parser.parse_args()
+
+
+for dir in iter(os.listdir(args.i)):
+    if dir != "src":
+        for content in os.listdir(args.i+'/'+dir):
+            if ".ism" in content and "final" in content:
+                content_path = args.i+'/'+dir+'/'+content
+                content_df = pd.read_csv(content_path, sep=" ", header=None)
+                if "actives" in content:
+                    content_df["active"] = np.ones([content_df.shape[0]])
+                elif "decoys" in content:
+                    content_df["active"] = np.zeros([content_df.shape[0]])
+            print(content_df.columns)
--- src/main.py
+++ src/main.py
@@ -7,27 +7,27 @@ from mol_graph import MolGraph, graph_from_mol, degrees
 
 
 # may not need array_rep_from_mol
-# def array_rep_from_mol(mol):
-#     """Precompute everything we need from MolGraph so that we can free the memory asap."""
-#     molgraph = graph_from_mol(mol)
-#     arrayrep = {'atom_features' : molgraph.feature_array('atom'),
-#                 'bond_features' : molgraph.feature_array('bond'),
-#                 'atom_list'     : molgraph.neighbor_list('molecule', 'atom'), # List of lists.
-#                 'rdkit_ix'      : molgraph.rdkit_ix_array()}  # For plotting only.
-#     for degree in degrees:
-#         # arrayrep[('atom_neighbors', degree)] = \
-#         #     np.array(molgraph.neighbor_list(('atom', degree), 'atom'), dtype=int)
-#         arrayrep[('bond_neighbors', degree)] = \
-#             np.array(molgraph.neighbor_list(('atom', degree), 'bond'), dtype=int)
-#     return arrayrep
+def array_rep_from_mol(mol):
+    """Precompute everything we need from MolGraph so that we can free the memory asap."""
+    molgraph = graph_from_mol(mol)
+    arrayrep = {'atom_features' : molgraph.feature_array('atom'),
+                'bond_features' : molgraph.feature_array('bond'),
+                'atom_list'     : molgraph.neighbor_list('molecule', 'atom'), # List of lists.
+                'rdkit_ix'      : molgraph.rdkit_ix_array()}  # For plotting only.
+    for degree in degrees:
+        # arrayrep[('atom_neighbors', degree)] = \
+        #     np.array(molgraph.neighbor_list(('atom', degree), 'atom'), dtype=int)
+        arrayrep[('bond_neighbors', degree)] = \
+            np.array(molgraph.neighbor_list(('atom', degree), 'bond'), dtype=int)
+    return arrayrep
 
 data = np.ndarray(0)
-# inf = open('/mounts/u-spa-d2/grad/derek/Downloads/abl1/actives_final.sdf','rb')
-inf = open('/home/derek/Downloads/actives_final.sdf','rb')
+inf = open('/mounts/u-spa-d2/grad/derek/Downloads/abl1/actives_final.sdf','rb')
 fsuppl = Chem.ForwardSDMolSupplier(inf)
 for mol in fsuppl:
     if mol is None:
         continue
     print(mol.GetNumAtoms())
     # data = np.vstack(rdmolops.GetAdjacencyMatrix(mol, useBO=True))
-    data = graph_from_mol(mol)
+    # data = graph_from_mol(mol)
+    data = array_rep_from_mol(mol)
