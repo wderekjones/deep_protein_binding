b3e8a700573e4966c8ecfe13c59de91f008bee50
williamdjones <wdjo224@g.uky.edu>
2018-01-19 17:14:41 -0500

    working on incorporating the GPU with half precision

--- src/label.py
+++ src/label.py
@@ -1,12 +1,11 @@
 import pandas as pd
-
+import numpy as np
 
 def read_labels(label_path, exclude_path):
 
     label_df = pd.read_csv(label_path, header=None)
     exclude_df = pd.read_csv(exclude_path, header=None)
-    label_df.drop(label_df.merge(exclude_df),axis=0, inplace=True)
-
+    return np.setdiff1d(label_df[0].values, exclude_df[0].values).tolist()
 
 if __name__ == "__main__":
     read_labels("/media/derek/Data/thesis_data/drug_features_list.csv", "/media/derek/Data/thesis_data/null_column_list.csv")
--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ import numpy as np
 from multiprocessing import cpu_count
 from MoleculeDataset import MoleculeDataset
 from torch.utils.data import DataLoader
-from models import LinearNetwork
+from model import LinearNetwork
+from label import read_labels
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-D', type=str, help="path to dataset")
@@ -14,8 +15,8 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
 
-
-    molecules = MoleculeDataset(args.D, ["MW","AMW"], nrows=10000, num_workers=cpu_count()-2)
+    labels = read_labels("/media/derek/Data/thesis_data/drug_features_list.csv", "/media/derek/Data/thesis_data/null_column_list.csv")
+    molecules = MoleculeDataset(args.D, labels, nrows=1000, num_workers=cpu_count()-2)
     molecules.read_csv_data()
     molecules.clean_csv_data()
     molecules.tensorize_smiles()
@@ -28,26 +29,26 @@ if __name__ == "__main__":
     atom_shape = molecules[0]['atom'].shape
     bond_shape = molecules[0]['bond'].shape
     edge_shape = molecules[0]['edge'].shape
-    output_dim = 2
+    output_dim = len(labels)
     model = LinearNetwork(batch_size=batch_size, atom_0=batch_size, atom_1=atom_shape[0], atom_2=atom_shape[1],
                       bond_0=batch_size, bond_1=bond_shape[0], bond_2=bond_shape[1], bond_3=bond_shape[2],
                       edge_0=batch_size, edge_1=edge_shape[0], edge_2=edge_shape[1],
                       hidden_00=10, hidden_01=10, hidden_02=10, hidden_10=batch_size*output_dim, num_outputs=output_dim)
 
-    # model.cuda()
+    model.cuda()
 
     optimizer = torch.optim.Adam(model.parameters())
     loss_fn = torch.nn.MSELoss()
-
+    print("training model")
     for epoch in range(num_epochs):
         epoch_losses = []
         optimizer.zero_grad()
         for i_batch, sample_batched in enumerate(molecule_loader):
-            pred = model(torch.autograd.Variable(sample_batched['atom'].float()),
-            torch.autograd.Variable(sample_batched['bond'].float()),
-            torch.autograd.Variable(sample_batched['edge'].float()))
-            loss = loss_fn(pred, torch.autograd.Variable(sample_batched['target'].float()))
+            pred = model(torch.autograd.Variable(sample_batched['atom'].type(torch.HalfTensor).cuda()),
+            torch.autograd.Variable(sample_batched['bond'].type(torch.HalfTensor).cuda()),
+            torch.autograd.Variable(sample_batched['edge'].type(torch.HalfTensor).cuda()))
+            loss = loss_fn(pred.cpu().type(torch.HalfTensor), torch.autograd.Variable(sample_batched['target'].type(torch.HalfTensor)))
             loss.backward()
-            epoch_losses.append(loss.data.numpy())
+            epoch_losses.append(loss.data.cpu().numpy())
         optimizer.step()
-    print("epoch: {} \t loss: {} ".format(epoch, np.mean(epoch_losses)))
+        print("epoch: {} \t loss: {} ".format(epoch, np.mean(epoch_losses)))
