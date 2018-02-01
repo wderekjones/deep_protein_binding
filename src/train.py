7e8f5b7ac15f7b089216cfa5bd2b1d0514979386
williamdjones <wdjo224@g.uky.edu>
2018-02-01 17:30:19 -0500

    changed the loss function to MSE, achieving better results, added an argument to specify learning rate. This commit gives a stable cpu implementation of the algorithm. Now to add GPU support and tensorboard.

--- src/train.py
+++ src/train.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, help="batch size to use in data loader", default=1)
     parser.add_argument("--n_epochs", type=int, help="number of training epochs", default=1)
     parser.add_argument("--use_cuda", type=bool, help="indicates whether to use gpu", default=False)
+    parser.add_argument("--lr", type=float, help="learning rate to use for training", default=1e-3)
     args = parser.parse_args()
 
     import torch.multiprocessing as mp
@@ -34,15 +35,18 @@ if __name__ == "__main__":
     if args.use_cuda:
         model.cuda()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    loss_fn = torch.nn.SmoothL1Loss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    loss_fn = torch.nn.MSELoss()
     print("training model")
 
     # data = MoleculeDatasetH5(data_dir="/mounts/u-vul-d1/scratch/wdjo224/data/deep_protein_binding/datasets", list_dir="/mounts/u-vul-d1/scratch/wdjo224/data/deep_protein_binding/dataset_compounds",
     #                              corrupt_path="/u/vul-d1/scratch/wdjo224/data/deep_protein_binding/corrupt_inputs.csv",targets=["label"],num_workers=1)
 
     molecules = MoleculeDatasetCSV(csv_file="/u/vul-d1/scratch/wdjo224/data/deep_protein_binding/kinase_no_duplicates_with_smiles.csv",
-                              corrupt_path="/u/vul-d1/scratch/wdjo224/data/deep_protein_binding/corrupt_inputs.csv", targets=["Hy"],cuda=args.use_cuda)
+                              corrupt_path="/u/vul-d1/scratch/wdjo224/data/deep_protein_binding/corrupt_inputs.csv", targets=["MLOGP"],cuda=args.use_cuda)
+
+    # TODO: may need to preprocess targets to ensure that no NANs, inf, etc; values are present
+
     print("size of dataset: {}".format(len(molecules)))
     from torch.utils.data import DataLoader
 
@@ -51,7 +55,7 @@ if __name__ == "__main__":
     num_workers = args.n_workers
     num_iters = int(np.ceil(len(molecules) / batch_size))
     molecule_loader = DataLoader(molecules, batch_size=batch_size, num_workers=num_workers, collate_fn=collate_fn)
-    print("batch size: {} \t num_iterations: {} \t num_workers: {}".format(batch_size, num_iters, num_workers))
+    print("batch size: {} \t learning rate: {} \t num_iterations: {} \t num_workers: {}".format(batch_size, args.lr, num_iters, num_workers))
     for epoch in range(0, epochs):
         epoch_loss = []
         epoch_preds = []
@@ -59,6 +63,7 @@ if __name__ == "__main__":
 
         # for idx, batch in tqdm(enumerate(molecule_loader), total=num_iters):
         for idx, batch in enumerate(molecule_loader):
+            optimizer.zero_grad()
             data = batch[0]
             y_pred = model(h=data["h"], g=data["g"])
             y_true = batch[0]["target"]
